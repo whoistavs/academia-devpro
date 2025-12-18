fb167daaddbba27bfb589e5b377a205ccf7c8949
whoistavs <octavio.marvel2018@gmail.com>
2025-12-18 15:59:32 -0300

    Fix server port binding

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const SECRET_KEY = "chave_secreta_super_segura"; // Em produção, usar .env
 
 // Configurar armazenamento do Multer
